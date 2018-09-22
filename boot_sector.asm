be3c2187191bc3143ae3b568f7457d399bfc52b8
Benji Tobias <benjitobias@gmail.com>
2018-09-22 21:20:34 +0300

    Access memory using offset

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,15 +1,22 @@
+[org 0x7c00] ; Auto calculate boot sector mem offset
 mov ah, 0x0e ; tty mode
-mov al, 'H'
-int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10
-mov al, 'o'
+;mov al, 'H'
+;int 0x10
+;mov al, 'e'
+;int 0x10
+;mov al, 'l'
+;int 0x10
+;int 0x10
+;mov al, 'o'
+;int 0x10
+
+mov al, [the_secret]
 int 0x10
 
 jmp $ ; jump to current address (infinite loop)
+
+the_secret:
+	db "X"
 
 ; padding and magic number indicating boot sector
 times 510-($-$$) db 0
