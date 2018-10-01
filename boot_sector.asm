d92866cfd6165984271c53705b248b861d0680bd
Benji Tobias <benjitobias@gmail.com>
2018-10-01 14:11:23 +0300

    Stack stuff

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,22 +1,41 @@
-[org 0x7c00] ; Auto calculate boot sector mem offset
 mov ah, 0x0e ; tty mode
-;mov al, 'H'
-;int 0x10
-;mov al, 'e'
-;int 0x10
-;mov al, 'l'
-;int 0x10
-;int 0x10
-;mov al, 'o'
-;int 0x10
 
-mov al, [the_secret]
+mov bp, 0x8000 ; far away from 0x7c00 so that we won't get overwritten
+mov sp, bp ; empty stack - sp = bp
+
+push 'A'
+push 'B'
+push 'C'
+
+; show the stack grows downwards
+mov al, [0x7ffe] ; 0x8000 - 2
+int 0x10
+
+; however, don't try to access [0x8000] now because it won't work
+; you can only access the stack top so, ath this point, only 0z7ffe
+mov al, [0x8000]
+int 0x10
+
+; recover characters using 'pop'
+; we can only pop full words to we need a register to manipulate the lower byte
+pop bx
+mov al, bl
+int 0x10 ; print "C"
+
+pop bx
+mov al, bl
+int 0x10 ; print "B"
+
+pop bx
+mov al, bl
+int 0x10 ; print "A"
+
+; data pop's is garbage now
+mov al, [0x8000]
 int 0x10
 
 jmp $ ; jump to current address (infinite loop)
 
-the_secret:
-	db "X"
 
 ; padding and magic number indicating boot sector
 times 510-($-$$) db 0
