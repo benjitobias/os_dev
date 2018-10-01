21d1a1d131f067b278c19162d3543f58df620db2
Benji Tobias <benjitobias@gmail.com>
2018-10-01 14:39:28 +0300

    Segmentation games

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,30 +1,29 @@
-[org 0x7c00] ; auto calculate offset
+mov ah, 0x0e ; tty
 
-; prepare params and then call functions
-mov bx, HELLO
-call print
-call print_nl
+mov al, [the_secret]
+int 0x10 ; we know this doesn't work already
 
-mov bx, GOODBYE
-call print
-call print_nl
+mov bx, [0x7c0] ; this segment is automatically <<4
+mov ds, bx
+; all memory references will now be offset by 'ds' implicitly
+mov al, [the_secret]
+int 0x10
 
-mov dx, "A"
-call print_hex
+mov al, [es:the_secret]
+int 0x10 ; doesn't look right. es currently 0x000
+
+mov bx, 0x7c0
+mov es, bx
+mov al, [es:the_secret]
+int 0x10
 
 
 jmp $ ; jump to current address (infinite loop)
 
-; include subroutines
-%include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
 
 ; data
-HELLO:
-	db 'Hello, World', 0
-
-GOODBYE:
-	db 'Goodbye', 0
+the_secret:
+	db 'X'
 
 
 ; padding and magic number indicating boot sector
